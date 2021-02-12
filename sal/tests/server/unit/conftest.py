b669447b3feea2268262dc706472217eef80f65d
tfarmer <tom.farmer@ukaea.uk>
2021-02-12 12:01:40 +0000

    Added server with authentication provider

--- sal/tests/server/unit/conftest.py
+++ sal/tests/server/unit/conftest.py
@@ -2,7 +2,8 @@ from unittest.mock import Mock, patch
 
 import pytest
 
-from sal.server.main import SALServer, PersistenceProvider
+from sal.server.main import (AuthenticationProvider, PersistenceProvider,
+                             SALServer)  
 
 @pytest.fixture
 def mock_persistence_provider():
@@ -10,6 +11,11 @@ def mock_persistence_provider():
     return pp 
 
 @pytest.fixture
+def mock_authentication_provider():
+    ap = Mock(spec_set=AuthenticationProvider)
+    ap.authenticate = (
+        lambda u, p : True if u == 'username' and p == 'password' else False) 
+    return ap 
 
 @pytest.fixture
 def server(mock_persistence_provider):
@@ -18,7 +24,26 @@ def server(mock_persistence_provider):
     A SALServer with mocked attributes 
     """
 
-    sal_server = SALServer(mock_persistence_provider)
-    sal_server.config['TESTING'] = True
-    with sal_server.app_context():
-        yield sal_server
+    ss = SALServer(mock_persistence_provider)
+    ss.config['TESTING'] = True
+    with ss.app_context():
+        yield ss
+
+@pytest.fixture
+def server_with_auth_prov(mock_persistence_provider,
+                          mock_authentication_provider):
+
+    """
+    A SALServer with a persistence_provider and an authentication_provider
+    """
+
+    ss = SALServer(mock_persistence_provider,
+                   authentication_provider=mock_authentication_provider,
+                   auth_token_secret="SECRET!"*10,
+                   auth_token_lifetime=10.,
+                   admin_enabled=True,
+                   admin_username='admin',
+                   admin_password='admin_password')
+    ss.config['TESTING'] = True
+    with ss.app_context():
+        yield ss
