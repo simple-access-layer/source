57be235b9958c954e7e63db8de8812f2b736bf4f
tfarmer <tom.farmer@ukaea.uk>
2021-02-23 11:15:27 +0000

    Added hypothesis strategies and implemented tests

--- sal/tests/core/unit/test_path.py
+++ sal/tests/core/unit/test_path.py
@@ -1,21 +1,112 @@
 
+from random import randint
+
 import pytest
-from hypothesis import given, strategies as st
+from hypothesis import example, given, strategies as st
 
 from sal.core import path
 
 
-def valid_path(request):
+# A segment can contain any lowercase alphanumeric characters, and '-', '_',
+# '.'
+SEGMENT_CHARACTERS = st.characters(min_codepoint=48,
+                                   max_codepoint=122,
+                                   blacklist_categories=['Lu', 'P', 'S'],
+                                   whitelist_characters=['-', '_', '.'])
 
-    pass
+# Almost all characters not in SEGMENT_CHARACTERS
+INVALID_CHARACTERS = st.characters(blacklist_categories=['Ll', 'Nd'],
+                                   blacklist_characters=['-', '_', '.'])
+
+@st.composite
+def int_postfix(draw):
+    
+    """
+    :return: A strategy with a str of the form ':*' where * is any non-negative
+    int
+    :rtype: LazyStrategy
+
+    .. note::
+        ints are restricted to <= 9999 for the sake of performance
+    """
+    
+    return ':{}'.format((draw(st.integers(0, 9999))))
+
+# A valid postfix for any path
+# This is either '', ':head' or ':*' where * is any non-negative int
+VALID_POSTFIX = st.one_of(st.just(''),  st.just(':head'), int_postfix())
+
+# A valid prefix for a relative path
+VALID_REL_PREFIX = st.one_of(st.just(''), st.just('../'))
 
 
+@st.composite
+def invalid_segment(draw):
+
+    """
+    :return: An invalid segment of a path, comprised of some valid segment
+    characters and at least 1 invalid character inserted at a random position
+    :rtype: LazyStrategy
+    """
+
+    valid_segment = draw(st.text(SEGMENT_CHARACTERS,
+                                 min_size=4,
+                                 max_size=8))
+    invalid_characters = draw(st.text(INVALID_CHARACTERS,
+                                      min_size=1,
+                                      max_size=4))
+
+    # Insert invalid characters randomly into valid segment
+    index = randint(0, len(valid_segment))
+    return valid_segment[:index] + invalid_characters + valid_segment[index:]
+
+
+@st.composite
+def valid_abs_path(draw):
+
+    """
+    :return: A valid absolute path consisting of one or more segments and a
+    valid path postfix
+    :rtype: LazyStrategy
+    """
+
+    n_segments = draw(st.integers(min_value=1, max_value=4))
+    valid_segment = st.text(SEGMENT_CHARACTERS,
+                            min_size=4,
+                            max_size=8)
+    valid_abs_path = '/' + '/'.join([draw(valid_segment) for i
+                                     in range(n_segments)])
+    if randint(0, 1):
+        valid_abs_path += draw(VALID_POSTFIX)
+    return valid_abs_path
+
+
+@st.composite
+def valid_rel_path(draw):
+
+    """
+    :return: A valid relative path consisting of one or more segments, a valid
+    relative path prefix and a valid path postfix
+    :rtype: LazyStrategy
+    """
+
+    n_segments = draw(st.integers(min_value=1, max_value=4))
+    valid_segment = st.text(SEGMENT_CHARACTERS,
+                            min_size=4,
+                            max_size=8)
+    valid_rel_path = '/'.join([draw(valid_segment) for i in range(n_segments)])
+    valid_rel_path = (draw(VALID_REL_PREFIX)
+                      + valid_rel_path
+                      + draw(VALID_POSTFIX))
+    return valid_rel_path
+
+
+@given(valid_segment=st.text(SEGMENT_CHARACTERS, min_size=1, max_size=15))
 def test_path_segment_validation(valid_segment):
 
     """
     GIVEN
-        A segment of any length consisting of alphanumeric characters, '-', '_'
-        and '.'
+        
     WHEN
         The segment is validated
     THEN
@@ -25,7 +116,23 @@ def test_path_segment_validation(valid_segment):
     assert path.segment_valid(valid_segment)
 
 
-def test_valid_path_validation():
+@given(invalid_segment=invalid_segment())
+def test_path_invalid_segment_validation(invalid_segment):
+
+    """
+    GIVEN
+        
+    WHEN
+        The segment is validated
+    THEN
+        The segment is determined to be valid
+    """
+
+    assert not path.segment_valid(invalid_segment)
+
+
+@given(valid_path=st.one_of(valid_abs_path(), valid_rel_path()))
+def test_valid_path_validation(valid_path):
 
     """
     GIVEN
@@ -37,6 +144,8 @@ def test_valid_path_validation():
     THEN
         The path is determined to be valid
     """
+
+    assert path.is_valid(valid_path)
 
 
 def test_path_decomposition():
@@ -61,7 +170,8 @@ def test_path_segment_normalisation():
     """
 
 
-def test_path_to_absolute():
+@given(valid_abs_path=valid_abs_path())
+def test_path_is_absolute(valid_abs_path):
 
     """
     GIVEN
@@ -71,3 +181,21 @@ def test_path_to_absolute():
     THEN
     """
 
+    assert path.is_absolute(valid_abs_path)
+
+
+@given(valid_rel_path=valid_rel_path())
+@example('.')
+@example('.:0')
+@example('.:head')
+def test_path_to_relative(valid_rel_path):
+
+    """
+    GIVEN
+
+    WHEN
+
+    THEN
+    """
+
+    assert path.is_relative(valid_rel_path)
