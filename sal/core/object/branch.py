629bfcb2f661e83d896209735787e6f77c13ca79
tfarmer <tom.farmer@ukaea.uk>
2021-03-11 15:41:39 +0000

    Move description property to Node

--- sal/core/object/branch.py
+++ sal/core/object/branch.py
@@ -1,5 +1,7 @@
+from .node import Node
 
-class Branch:
+
+class Branch(Node):
     """
     An object representing a data tree branch node.
 
@@ -9,22 +11,7 @@ class Branch:
     """
 
     def __init__(self, description):
-        self.description = str(description)
-
-    @property
-    def description(self):
-        """
-        A string describing the branch contents.
-        """
-        return self._description
-
-    @description.setter
-    def description(self, value):
-        if not isinstance(value, str):
-            raise ValueError("Description must be a string.")
-        if not value:
-            raise ValueError("A description cannot be an empty string.")
-        self._description = value
+        super().__init__(description)
 
     def to_dict(self):
         """
@@ -45,3 +32,5 @@ class Branch:
         """
 
         return cls(d['description'])
+
+    
--- sal/core/object/node.py
+++ sal/core/object/node.py
@@ -4,7 +4,22 @@ class Node(ABC):
 
     def __init__(self, description):
 
-        self.description = str(description)
+        self.description = description
+
+    @property
+    def description(self):
+        """
+        A string describing the branch contents.
+        """
+        return self._description
+
+    @description.setter
+    def description(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Description must be a string.")
+        if not value:
+            raise ValueError("A description cannot be an empty string.")
+        self._description = value
 
     @abstractmethod
     def serialise(self, additional):
